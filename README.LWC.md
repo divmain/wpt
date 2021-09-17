c76ca90a5a22f37eea12f3de847c51290f1d73a6
Dale Bustad <dale@divmain.com>
2021-09-17 13:43:15 -0700

    Add LWC script.

--- README.LWC.md
+++ README.LWC.md
@@ -31,7 +31,7 @@ Run tests:
 If the script can't find your Chrome binary, try:
 
 ```bash
-./wpt run --binary "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --install-webdriver --test-types testharness --skip-timeout chrome --log-html ./report.html | tee /dev/tty > ./stdout.txt
+./wpt run --binary "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --install-webdriver --test-types testharness --skip-timeout chrome --log-html ./report.html | tee /dev/tty 2>&1 > ./stdout.txt
 ```
 
 Install `chromedriver` if prompted.
---
+++ lwc/transform-html.js
@@ -0,0 +1,75 @@
+import { opendir, writeFile } from 'fs/promises';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { exec as _exec } from 'child_process';
+import { promisify } from 'util';
+
+const exec = promisify(_exec);
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const rootPath = path.resolve(__dirname, '..');
+//                  <script src="/resources/testharness.js"></script>
+const harnessTag = '<script src="/resources/testharness.js"></script>';
+const syntheticShadowTag = '<script src="/resources/synthetic-shadow.js"></script>\n';
+
+async function* findHtmlFiles (basePath) {
+  const directory = await opendir(basePath);
+
+  for await (const dirEntry of directory) {
+    const fullPath = path.join(basePath, dirEntry.name);
+    if (dirEntry.isDirectory()) {
+      yield* findHtmlFiles(fullPath);
+    }
+    if (dirEntry.isFile() && dirEntry.name.endsWith('.html')) {
+      yield fullPath;
+    }
+  }
+}
+
+async function* getPathPairs (rootPath, fullPaths) {
+  for await (const fullPath of fullPaths) {
+    yield [
+      path.relative(rootPath, fullPath),
+      fullPath,
+    ];
+  }
+}
+
+async function getSourceFromMaster (relPath) {
+  try {
+    const { stdout } = await exec(`git show master:${relPath}`, {
+      cwd: rootPath,
+      shell: true,
+    });
+    return stdout;
+  } catch (err) {
+    if (err.stderr) {
+      throw new Error(err.stderr);
+    } else {
+      throw new Error(`Unknown error for file: ${relPath}`)
+    }
+  }
+}
+
+function insertSyntheticShadowDependency (originalSource, relPath) {
+  const harnessTagIdx = originalSource.indexOf(harnessTag);
+  if (harnessTagIdx === -1) {
+    return originalSource;
+  }
+  return `${originalSource.slice(0, harnessTagIdx)}${syntheticShadowTag}${originalSource.slice(harnessTagIdx)}`;
+}
+
+(async function main () {
+  const htmlFileFullPaths = findHtmlFiles(rootPath)
+  for await (const [relPath, fullPath] of getPathPairs(rootPath, htmlFileFullPaths)) {
+    process.stdout.write(`processing ${relPath}... `);
+    try {
+      const originalSource = await getSourceFromMaster(relPath);
+      const newSource = insertSyntheticShadowDependency(originalSource, relPath);
+      await writeFile(fullPath, newSource);
+      console.log(originalSource === newSource ? 'skipped.' : 'done.');
+    } catch (err) {
+      console.log(err)
+    }
+  }
+})();
